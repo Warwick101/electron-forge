349da311525d8c053e09872b5af26bc5afdfd059
Warwick de Kock <war.wick101@gmail.com>
2020-07-11 13:33:36 -0500

    add auto-launch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+const AutoLaunch = require('auto-launch');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -24,6 +25,15 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+
+
+let autoLaunch = new AutoLaunch({
+    name: 'Your app name goes here',
+    path: app.getPath('exe'),
+});
+autoLaunch.isEnabled().then((isEnabled: any) => {
+    if (!isEnabled) autoLaunch.enable();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
