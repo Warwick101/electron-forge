4e13104a42509b6ed24984ffb6ff72ac3584acbc
Warwick de Kock <war.wick101@gmail.com>
2020-07-11 14:19:05 -0500

    put back auto-launch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+const AutoLaunch = require('auto-launch');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -25,11 +26,23 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
+// Out of box electron not working for linux...yet
 
-app.setLoginItemSettings({
-    openAtLogin: true,
-    path: app.getPath("exe")
+// app.setLoginItemSettings({
+//     openAtLogin: true,
+//     path: app.getPath("exe")
+// });
+
+// Using auto-launch as workaround
+
+let autoLaunch = new AutoLaunch({
+    name: 'Your app name goes here',
+    path: app.getPath('exe'),
 });
+autoLaunch.isEnabled().then((isEnabled: any) => {
+    if (!isEnabled) autoLaunch.enable();
+});
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
